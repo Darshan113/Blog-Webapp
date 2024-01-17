73b12c79768009c8836563fd6bbeacf6d300175b
Darshan Panchal <dppanchal106@gmail.com>
2024-01-17 17:38:48 +0530

    password hashing

--- server/routes/Authentication.route.js
+++ server/routes/Authentication.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { AuthenticationController } = require('../controllers');
+const  validate  = require('../middlewares/validate');
+const { createUser } = require('../validations/user.validation');
 
 const router = express.Router()
 
-router
-.post('/register',AuthenticationController.Register)
+router.post('/register',validate(createUser),AuthenticationController.Register)
 
 
 module.exports =  router
