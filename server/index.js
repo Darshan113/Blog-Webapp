02b19b7a5cc476b58e289ad128b2d29581611369
Darshan Panchal <dppanchal106@gmail.com>
2024-01-17 11:31:05 +0530

    setup server and db connection

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const { Authentication } = require('./routes');
+const { connectDB } = require('./config/db');
+const morgan = require('morgan');
 
 const app = express()
+const port = 8000
+connectDB()
 
-app.route('/').get((req,res)=>{
-    res.send('hello get')
-}).post((req,res)=>{
-    res.send('hello post')
-}).put((req,res)=>{
-    res.send('hello put')
-}).delete((req,res)=>{
-    res.send('hello delete')
+// middleware
+app.use(morgan('dev'))
+
+// routes
+app.use('/api/v1',Authentication)
+
+app.get('/',(req,res)=>{
+    res.send('hello server')
 })
 
-app.listen(8000,(er)=>{
+app.listen(port,(er)=>{
     console.log(er ? er : 'server started at port 8000',)
 })
