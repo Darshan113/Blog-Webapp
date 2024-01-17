26d4fa822752b762c5903dc684f137394be98977
Darshan Panchal <dppanchal106@gmail.com>
2024-01-17 10:48:04 +0530

    learn rest

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log('jell')
+const express = require('express');
+
+const app = express()
+
+app.route('/').get((req,res)=>{
+    res.send('hello get')
+}).post((req,res)=>{
+    res.send('hello post')
+}).put((req,res)=>{
+    res.send('hello put')
+}).delete((req,res)=>{
+    res.send('hello delete')
+})
+
+app.listen(8000,(er)=>{
+    console.log(er ? er : 'server started at port 8000',)
+})
